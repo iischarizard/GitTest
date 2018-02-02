1b54b031c057966815f7551d181d48ecad399d92
iischarizard <zlnorman10@gmail.com>
2018-02-01 22:17:57 -0800

    ohya

--- TESTERINO.java
+++ TESTERINO.java
@@ -2,6 +2,11 @@ public class TESTERINO{
 
 	public static void main(String[] args){
 		System.out.println("Bye bye World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
